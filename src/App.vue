94d80704d28f8ea740405c22f6eab2068ddc566b
skarner2016 <skarner2016@gmail.com>
2023-12-14 21:47:07 +0800

    learn vue3

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import reaactRef from './components/tutorial/1_reactive_ref.vue'
   import computed from './components/tutorial/2_computed.vue'
   import watch from './components/tutorial/3_watch.vue'
+  import props from './components/tutorial/4_props.vue'
 
 </script>
 
@@ -9,4 +10,5 @@
   <reaactRef></reaactRef>
   <computed></computed>
   <watch></watch>
+  <props></props>
 </template>
