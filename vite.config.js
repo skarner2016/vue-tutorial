7c300e63f5c7d23e9b627fa6ee6431ba010d617d
skarner2016 <skarner2016@gmail.com>
2023-12-28 22:38:32 +0800

    feat: 主题色定制

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 1. 配置elementPlus采用saas样式配色系统
+      resolvers: [ElementPlusResolver({ importStyle: "sass"})],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
